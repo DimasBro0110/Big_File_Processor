96983db1a5a0b2e6ca948878b24f765195001361
DimasBro0110 <dimasgitacc@gmail.com>
2015-10-25 14:47:14 +0200

    Был исправлен Генератор(Сервер) задач, добавлен класс Table, для хранения сортированных аннаграмм, написан Обработчик(Клиент).

--- BigFileProcess/Program.cs
+++ BigFileProcess/Program.cs
@@ -22,7 +22,7 @@ namespace BigFileProcess
             private bool isDone;
             private string fileFrom;
             private static Queue<string> tasks;
-            private static Dictionary<int, string> annagrams;
+            private static Table annagrams;
             private static Mutex task_mutex;
             private static Mutex angr_mutex;
             private static Mutex proc_mutex;
@@ -31,14 +31,19 @@ namespace BigFileProcess
             public Generator(string path)
             {
                 isDone = false;
+                IPAddress adress;
+                IPAddress.TryParse("127.0.0.5", out adress);
+                tcpListener = new TcpListener(adress, 30);
+                Console.WriteLine("Server launched, adress {0}, port {1}", adress.ToString(), 30);
                 fileFrom = path;
                 task_mutex = new Mutex();
                 angr_mutex = new Mutex();
                 proc_mutex = new Mutex();
                 tasks = new Queue<string>();
-                annagrams = new Dictionary<int, string>();
+                annagrams = new Table();
                 Thread t = new Thread(Tasks_Getter);
                 t.Start();
+                Listener();
             }
 
             private void Listener()
@@ -62,7 +67,7 @@ namespace BigFileProcess
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine(ex);
+                    Console.WriteLine("Client Disconnected \n{0}", ex);
                     if (stream != null)
                     {
                         stream.Dispose();
@@ -76,42 +81,94 @@ namespace BigFileProcess
                 }
             }
 
+            private string Resize(byte[] array)
+            {
+                byte[] cur;
+                int i = 0;
+                foreach (byte b in array)
+                {
+                    if (b != '\0' && b != '\n')
+                    {
+                        i++;
+                    }
+                }
+                cur = new byte[i];
+                i = 0;
+                foreach (byte b in array)
+                {
+                    if (b != '\0' && b != '\n')
+                    {
+                        cur[i] = b;
+                        i++;
+                    }
+                }
+                return Encoding.ASCII.GetString(cur, 0, cur.Length);
+            }
+
             private void Task_To_Process(NetworkStream stream)
             {
                 int countReadBytes = 0;
                 byte[] ReadBytes = new byte[2048];
+                byte[] SendBytes = new byte[2048];
                 string recieved = "";
 
-                task_mutex.WaitOne();
-                byte[] SendBytes = Encoding.ASCII.GetBytes(tasks.Dequeue());
-                task_mutex.ReleaseMutex();
-
-                proc_mutex.WaitOne();
-                stream.Write(SendBytes, 0, SendBytes.Length);
-                proc_mutex.ReleaseMutex();
-
-                Array.Clear(ReadBytes, 0, ReadBytes.Length);
-
-                while ((countReadBytes = stream.Read(ReadBytes, 0, ReadBytes.Length)) > 0)
+                if (tasks.Count != 0)
                 {
-                    recieved = Encoding.ASCII.GetString(ReadBytes, 0, ReadBytes.Length);
-                    string[] result = recieved.Split(':');
-
-                    angr_mutex.WaitOne();
-                    annagrams.Add(Convert.ToInt32(result[0]), result[1]);
-                    angr_mutex.ReleaseMutex();
-
-                    Array.Clear(result, 0, result.Length);
-
                     task_mutex.WaitOne();
-                    ReadBytes = Encoding.ASCII.GetBytes(tasks.Dequeue());
+                    SendBytes = Encoding.ASCII.GetBytes(tasks.Dequeue());
                     task_mutex.ReleaseMutex();
 
                     proc_mutex.WaitOne();
-                    stream.Write(ReadBytes, 0, ReadBytes.Length);
+                    stream.Write(SendBytes, 0, SendBytes.Length);
                     proc_mutex.ReleaseMutex();
+                }
+                else
+                {
+                    SendBytes = Encoding.ASCII.GetBytes("closed -1");
 
-                    Array.Clear(ReadBytes, 0, ReadBytes.Length);
+                    proc_mutex.WaitOne();
+                    stream.Write(SendBytes, 0, SendBytes.Length);
+                    proc_mutex.ReleaseMutex();
+                }
+
+                Array.Clear(SendBytes, 0, SendBytes.Length);
+
+                while ((countReadBytes = stream.Read(ReadBytes, 0, ReadBytes.Length)) > 0)
+                {
+                    recieved = Resize(ReadBytes);
+                    if (!recieved.Equals("closed -1"))
+                    {
+                        string[] result = recieved.Split(':');
+                        angr_mutex.WaitOne();
+                        annagrams.setValues(Convert.ToInt32(result[0]), result[1]);                       
+                        angr_mutex.ReleaseMutex();
+
+                        Array.Clear(result, 0, result.Length);
+
+                        if (tasks.Count != 0)
+                        {
+                            task_mutex.WaitOne();
+                            SendBytes = Encoding.ASCII.GetBytes(tasks.Dequeue());
+                            task_mutex.ReleaseMutex();
+                        }
+                        else
+                        {
+                            SendBytes = Encoding.ASCII.GetBytes("closed -1");
+                        }
+
+                        proc_mutex.WaitOne();
+                        stream.Write(SendBytes, 0, SendBytes.Length);
+                        proc_mutex.ReleaseMutex();
+
+                        Array.Clear(ReadBytes, 0, ReadBytes.Length);
+                        Array.Clear(SendBytes, 0, SendBytes.Length);
+                    }
+                    else
+                    {
+                        SendBytes = Encoding.ASCII.GetBytes("closed -1");
+
+                        stream.Write(SendBytes, 0, SendBytes.Length);
+                    }
                 }
             }
 
@@ -123,15 +180,15 @@ namespace BigFileProcess
                     try
                     {
                         string task_str = "";
-                        sr = new StreamReader(fileFrom);
+                        Encoding enc = Encoding.GetEncoding(1251);
+                        sr = new StreamReader(fileFrom, enc);
                         while (sr.Peek() >= 0)
                         {
                             task_str = sr.ReadLine();
-                            task_mutex.WaitOne();
                             tasks.Enqueue(task_str);
-                            task_mutex.ReleaseMutex();
                         }
                         isDone = true;
+                        Console.WriteLine("Done {0}", tasks.Count);
                     }
                     catch (Exception ex)
                     {
@@ -151,12 +208,41 @@ namespace BigFileProcess
 
             ~Generator()
             {
+                tasks.Clear();
             }
         }
 
         static void Main(string[] args)
         {
+            int max = Environment.ProcessorCount * 2;
+            ThreadPool.SetMaxThreads(max, max);
+            ThreadPool.SetMinThreads(2, 2);
+            new Generator(@"C:\Architect\file.txt");
             Console.ReadKey();
+        }
+    }
+
+    class Table
+    {
+        private Dictionary<int, LinkedList<string>> table;
+
+        public Table()
+        {
+            table = new Dictionary<int, LinkedList<string>>();
+        }
+
+        public void setValues(int hash, string word)
+        {
+            if (this.table.ContainsKey(hash))
+            {
+                this.table[hash].AddLast(word);
+            }
+            else
+            {
+                LinkedList<string> lst = new LinkedList<string>();
+                lst.AddLast(word);
+                this.table.Add(hash, lst);
+            }
         }
     }
 }
---
+++ BigFileProsess_Client/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BigFileProsess_Client
+{
+    class Processor
+    {
+        private TcpClient client;
+
+        public Processor()
+        {
+            try
+            {
+                client = new TcpClient("127.0.0.5", 30);
+                if (client.Connected)
+                {
+                    ServerConnection(client.GetStream());
+                }
+                else
+                {
+                    client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private string TaskToDo(string task)
+        {
+            int hashsum = 1;
+            if (task.Equals("closed -1"))
+            {
+                return "closed -1";
+            }
+            else
+            {
+                foreach (char ch in task.ToLower())
+                {
+                    hashsum *= ch;
+                }
+            }
+            string res = Convert.ToString(hashsum) + ":" + task;
+
+            return res; 
+        }
+
+        private string Resize(byte[] array)
+        {
+            byte[] cur;
+            int i = 0;
+            foreach (byte b in array)
+            {
+                if (b != '\0' && b!= '\n')
+                {
+                    i++;
+                }
+            }
+            cur = new byte[i];
+            i = 0;
+            foreach (byte b in array)
+            {
+                if (b != '\0' && b!= '\n')
+                {
+                    cur[i] = b;
+                    i++;
+                }
+            }
+            return Encoding.ASCII.GetString(cur, 0, cur.Length);
+        }
+
+        private void ServerConnection(NetworkStream stream)
+        {
+            int RecievedSymbols = 0;
+            byte[] recieved = new byte[2048];
+            byte[] to_send = new byte[2048];
+            string task = "";
+            while ( (RecievedSymbols = stream.Read(recieved, 0, recieved.Length)) > 0 )
+            {
+                task = Resize(recieved);
+                task = TaskToDo(task);
+                Console.WriteLine(task);
+                if (task.Equals("closed -1"))
+                {
+                    to_send = Encoding.ASCII.GetBytes(task);                   
+                    stream.Write(to_send, 0, to_send.Length);
+                    client.Close();
+                    return;
+                }
+                else
+                {
+                    to_send = Encoding.ASCII.GetBytes(task);
+                    stream.Write(to_send, 0, to_send.Length);
+                }
+               Array.Clear(to_send, 0, to_send.Length);
+               Array.Clear(recieved, 0, recieved.Length);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Processor();
+            Console.ReadKey();
+        }
+    }
+}
